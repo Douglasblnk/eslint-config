6f0e44ab2d22a01828c4ce435cf8d9d6ead9f8a2
douglasblnk <douglasblank1@gmail.com>
2021-05-26 23:10:59 -0300

    feat: added eslint rules

---
+++ packages/vue/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    '@douglasblnk/eslint-config-ts',
+  ],
+  rules: {
+    'vue/html-self-closing': 'off',
+    'vue/no-v-html': 'off',
+  },
+}
