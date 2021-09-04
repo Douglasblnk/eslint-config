749687568664de89cce9dcd31f60f3dba9af1040
Douglas Blank <douglasblank1@gmail.com>
2021-09-04 09:58:48 -0300

    feat: updated some packages

---
+++ packages/js/index.js
@@ -0,0 +1,69 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:jsonc/recommended-with-jsonc',
+    'plugin:yml/standard',
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: { extensions: ['.js', '.mjs', '.ts', '.d.ts'] },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.json', '*.json5'],
+      parser: 'jsonc-eslint-parser',
+      rules: {
+        'quotes': ['error', 'double'],
+        'quote-props': ['error', 'always'],
+        'comma-dangle': ['error', 'never'],
+      },
+    },
+    {
+      files: ['package.json'],
+      parser: 'jsonc-eslint-parser',
+      rules: {
+        'jsonc/sort-keys': [
+          'error',
+          {
+            pathPattern: '^$',
+            order: [
+              'name',
+              'version',
+              'description',
+              'keywords',
+              'license',
+              'repository',
+              'funding',
+              'author',
+              'type',
+              'files',
+              'exports',
+              'main',
+              'module',
+              'unpkg',
+              'bin',
+              'scripts',
+              'husky',
+              'lint-staged',
+              'peerDependencies',
+              'peerDependenciesMeta',
+              'dependencies',
+              'devDependencies',
+              'eslintConfig',
+            ],
+          },
+          {
+            pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies$',
+            order: { type: 'asc' },
+          },
+        ],
+      },
+    },
+  ],
+}
